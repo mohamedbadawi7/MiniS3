ddc69ab7b72a3895bdc8e120a63b05756db15885
mohamedbadawi7 <mohamed.badawi@usask.ca>
2025-05-16 14:29:35 +0300

    Initialized MiniS3 proejct

---
+++ src/test/java/com/example/MiniS3/MiniS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MiniS3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
